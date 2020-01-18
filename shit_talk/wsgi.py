8591cb741952e88874d9c6813fce47f17b17a46d
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-01-19 05:08:59 +0800

    doned

--- shit_talk/wsgi.py
+++ shit_talk/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shit_talk_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shit_talk.settings')
 
 application = get_wsgi_application()
